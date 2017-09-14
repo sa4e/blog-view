b9efda144d6e50c8b056dd998ada3306e2fbcfcc
KRYST4L <hasaigive@gmail.com>
2017-09-14 23:21:37 +0800

    update

--- src/test/java/cn/sa4e/blog/service/test/TestUserService.java
+++ src/test/java/cn/sa4e/blog/service/test/TestUserService.java
@@ -12,6 +12,7 @@ import com.alibaba.dubbo.config.annotation.Reference;
 
 import cn.sa4e.blog.model.User;
 import cn.sa4e.blog.service.IUserService;
+import cn.sa4e.blog.utils.MD5Utils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,13 +23,22 @@ public class TestUserService {
 	
 	@Test
 	public void testInsert() throws Exception{
-		User user = new User("kryst4l", "123", "hasaigive@gmail.com");
-		userService.insert(user);
+		//User user = new User("kryst4l", "123", "hasaigive@gmail.com");
+		//userService.insert(user);
 	}
 	
 	@Test
 	public void testGetAllUser() throws Exception{
 		List<User> userList = userService.getAllUser();
 		Assert.assertEquals(userList.size(),4);
+	}
+	
+	//初始化root权限信息
+	@Test
+	public void initUser() throws Exception{
+		String password = "laiI.1.II.11.";
+		password = MD5Utils.md5(password);
+		User user = new User("sa4e", password, null);
+		userService.insert(user);
 	}
 }
